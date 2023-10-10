f8d461b62cddcee0089632f9478a90e0efb5c514
Sukhman50 <140614380+Sukhman50@users.noreply.github.com>
2023-10-10 14:55:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var fname="Sukhmanjeet";
+console.log ("Firstname: "+fname);
+
+var lname="Singh";
+console.log ("Lastname: "+lname);
+
+var fullname=fname.concat(lname);
+console.log ("Fulname:"+fullname);
+
+var length_fullname=fullname.length;
+console.log ("Length of Fullname:"+length_fullname);
+
+var sports="Football";
+console.log ("Oldsport: "+sports);
+
+var new_name=sports.replace ("Foot","basket");
+console.log ("Newsport: "+new_name);
+
+var to_upper=sports.toUpperCase ();
+console.log ("Toupper: "+to_upper);
+
+var to_lower=sports.toLowerCase ();
+console.log ("Tolower: "+to_lower);
+
+var number1=1000;
+console.log ("Num1:"+number1);
+
+var number2=500;
+console.log ("Num2:"+number2);
+
+var total=number1+number2;
+console.log ("Total:"+total);
+
+var diff=number1-number2;
+console.log ("Difference:"+diff);
+
+var div=number1/number2;
+console.log ("Division:"+div);
+
+var multi=number1*number2;
+console.log ("Multiplication:"+multi);
+
+var max_num=Math.max (number1,number2);
+console.log ("Max number:"+max_num);
+
+var min_num=Math.min (number1,number2);
+console.log ("Minimum value:"+min_num);
+
+var power_four=Math.pow (4,2);
+console.log ("Power value:"+power_four);
+
+var square=Math.sqrt (144);
+console.log ("Square Root:"+square);
